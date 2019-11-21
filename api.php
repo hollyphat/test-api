39fa2ce0361fa523677d55a38ad1ba5253d0ab1f
Olatunji <sirhollyfat@gmail.com>
2019-11-21 12:25:36 +0100

    Try login

--- api.php
+++ api.php
@@ -8,5 +8,13 @@
 	if(isset($_POST['ok-login'])){
 		$username = trim(addslashes($_POST['username']));
 		$password = trim(addslashes($_POST['password']));
+
+		$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+		$sql = mysqli_query("$query", $connection);
+
+		$rows = mysqli_num_rows($sql);
+
+		var_dump($rows);
 	}
 ?>
