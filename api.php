4db2091f43502318509b17e02561c02946c463ae
Olatunji <sirhollyfat@gmail.com>
2019-11-21 12:18:55 +0100

    initial commit

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+	$db_host = "localhost";
+	$db_name = "api";
+	$db_user = "root";
+	$db_pass = "";
+	$connection = mysqli_connect($db_host,$db_user, $db_pass, $db_name) or die(mysqli_error());
+?>
