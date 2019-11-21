05814d03e1f9562a7897fb25ab1805f1366f612e
Olatunji <sirhollyfat@gmail.com>
2019-11-21 12:23:16 +0100

    Accept login details

--- api.php
+++ api.php
@@ -5,5 +5,8 @@
 	$db_pass = "";
 	$connection = mysqli_connect($db_host,$db_user, $db_pass, $db_name) or die(mysqli_error());
 
-	
+	if(isset($_POST['ok-login'])){
+		$username = trim(addslashes($_POST['username']));
+		$password = trim(addslashes($_POST['password']));
+	}
 ?>
